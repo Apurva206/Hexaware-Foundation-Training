12cfba28d0e651867114f0bf9e4c84e1312c8538
Apurva Singhal <91753893+Apurva206@users.noreply.github.com>
2024-10-09 23:19:52 +0530

    Updated Assignment Submission

---
+++ C# Assignment/BankingSystem.BusinessLayer/Repository/AccountRepository.cs
@@ -0,0 +1,26 @@
+﻿using BankingSystem.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BankingSystem.DAO.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private List<Account> accounts = new List<Account>();
+
+        public void AddAccount(Account account)
+        {
+            accounts.Add(account);
+        }
+
+        public Account GetAccountById(int accountId)
+        {
+            return accounts.FirstOrDefault(a => a.AccountId == accountId);
+        }
+
+        public IEnumerable<Account> GetAllAccounts()
+        {
+            return accounts;
+        }
+    }
+}
