6d983007b71f5d98d5c2f7c0c86e19a79236b362
Apurva Singhal <91753893+Apurva206@users.noreply.github.com>
2024-10-14 22:33:41 +0530

    Add files via upload

---
+++ C# Assignment/BankingSystem.BusinessLayer/Repository/AccountRepository.cs
@@ -0,0 +1,151 @@
+﻿using BankingSystem.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using BankingSystem.Exceptions; // Ensure to include custom exceptions
+
+namespace BankingSystem.DAO.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private List<Account> accounts = new List<Account>();
+
+        // Adds a new account to the repository
+        public void AddAccount(Account account)
+        {
+            accounts.Add(account);
+        }
+
+        // Creates a new account for a customer with specified type and balance
+        public void CreateAccount(int customerId, string accType, double balance)
+        {
+            var account = new Account
+            {
+                CustomerId = customerId, 
+                AccountType = accType,
+                Balance = (decimal)balance
+            };
+            AddAccount(account);
+        }
+
+        // Deposits a specified amount into the account identified by accountNumber
+        public void Deposit(long accountNumber, decimal amount)
+        {
+            var account = GetAccountById(accountNumber);
+            if (account == null)
+            {
+                throw new InvalidAccountException($"Account with ID {accountNumber} does not exist.");
+            }
+            account.Balance += (decimal)amount; // Increase balance
+            UpdateAccount(account); // Update the account in the repository
+        }
+
+        // Withdraws a specified amount from the account identified by accountNumber
+        public void Withdraw(long accountNumber, decimal amount)
+        {
+            var account = GetAccountById(accountNumber);
+            if (account == null)
+            {
+                throw new InvalidAccountException($"Account with ID {accountNumber} does not exist.");
+            }
+
+            if (account.Balance < (decimal)amount)
+            {
+                throw new InsufficientFundException($"Withdrawal amount {amount} exceeds the available balance of {account.Balance}.");
+            }
+
+            account.Balance -= (decimal)amount; // Decrease balance
+            UpdateAccount(account); // Update the account in the repository
+        }
+
+        // Transfers amount from one account to another
+        public void Transfer(long fromAccountNumber, long toAccountNumber, decimal amount)
+        {
+            var fromAccount = GetAccountById(fromAccountNumber);
+            var toAccount = GetAccountById(toAccountNumber);
+
+            if (fromAccount == null)
+            {
+                throw new InvalidAccountException($"Source account with ID {fromAccountNumber} does not exist.");
+            }
+
+            if (toAccount == null)
+            {
+                throw new InvalidAccountException($"Destination account with ID {toAccountNumber} does not exist.");
+            }
+
+            if (fromAccount.Balance < amount)
+            {
+                throw new InsufficientFundException($"Transfer amount {amount} exceeds the available balance of {fromAccount.Balance}.");
+            }
+
+            // Perform transfer
+            fromAccount.Balance -= amount;
+            toAccount.Balance += amount;
+
+            UpdateAccount(fromAccount); // Update source account
+            UpdateAccount(toAccount); // Update destination account
+        }
+
+        // Gets the balance of an account identified by accountNumber
+        public float GetAccountBalance(long accountNumber)
+        {
+            var account = GetAccountById(accountNumber);
+            if (account == null)
+            {
+                throw new InvalidAccountException($"Account with ID {accountNumber} does not exist.");
+            }
+            return (float)account.Balance; // Return the balance as float
+        }
+
+        // Gets an account by its integer ID
+        public Account GetAccountById(int accountId)
+        {
+            return accounts.FirstOrDefault(a => a.AccountId == accountId);
+        }
+
+        // Gets an account by its long account number
+        public Account GetAccountById(long accountNumber)
+        {
+            return accounts.FirstOrDefault(a => a.AccountId == accountNumber); // Assuming AccountId is being used as account number
+        }
+
+        // Retrieves account details
+        public object GetAccountDetails(long accountNumber)
+        {
+            var account = GetAccountById(accountNumber);
+            if (account == null)
+            {
+                throw new InvalidAccountException($"Account with ID {accountNumber} does not exist.");
+            }
+            return account; // Return account details
+        }
+
+        // Gets all accounts in the repository
+        public IEnumerable<Account> GetAllAccounts()
+        {
+            return accounts;
+        }
+
+        // Updates the account details in the repository
+        public void UpdateAccount(Account account)
+        {
+            var existingAccount = GetAccountById(account.AccountId);
+            if (existingAccount != null)
+            {
+                existingAccount.Balance = account.Balance; // Update the balance
+                existingAccount.AccountType = account.AccountType; // Update account type
+                // Update any other necessary properties here
+            }
+            else
+            {
+                throw new InvalidAccountException($"Account with ID {account.AccountId} does not exist.");
+            }
+        }
+
+        // Lists all accounts in the repository
+        public List<Account> ListAccounts()
+        {
+            return accounts.ToList(); // Return a list of all accounts
+        }
+    }
+}
---
+++ C# Assignment/BankingSystem.BusinessLayer/Services/AccountService.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using BankingSystem.Entities;
+using BankingSystem.Exceptions;
+using BankingSystem.DAO.Repository;
+
+namespace BankingSystem.DAO.Service
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository accountRepository;
+
+        public AccountService(IAccountRepository accountRepo)
+        {
+            accountRepository = accountRepo;
+        }
+
+        public void CreateAccount(Account account)
+        {
+            accountRepository.AddAccount(account);
+        }
+
+        public Account GetAccount(int accountId)
+        {
+            var account = accountRepository.GetAccountById(accountId);
+            if (account == null)
+            {
+                throw new InvalidAccountException($"Account with ID {accountId} does not exist.");
+            }
+            return account;
+        }
+
+        public IEnumerable<Account> GetAllAccounts()
+        {
+            return accountRepository.GetAllAccounts();
+        }
+
+        public void Withdraw(int accountId, decimal amount)
+        {
+            var account = GetAccount(accountId);
+            if (account.Balance < amount)
+            {
+                throw new InsufficientFundException($"Withdrawal amount {amount} exceeds the available balance of {account.Balance}.");
+            }
+
+            account.Balance -= amount;
+            accountRepository.UpdateAccount(account); // Ensure the updated balance is saved
+        }
+
+        public void Transfer(int fromAccountId, int toAccountId, decimal amount)
+        {
+            var fromAccount = GetAccount(fromAccountId);
+            var toAccount = GetAccount(toAccountId);
+
+            if (fromAccount.Balance < amount)
+            {
+                throw new InsufficientFundException($"Transfer amount {amount} exceeds the available balance of {fromAccount.Balance}.");
+            }
+
+            fromAccount.Balance -= amount;
+            toAccount.Balance += amount;
+
+            accountRepository.UpdateAccount(fromAccount); // Save changes to both accounts
+            accountRepository.UpdateAccount(toAccount);
+        }
+
+        public void WithdrawWithOverdraft(int accountId, decimal amount, decimal overdraftLimit)
+        {
+            var account = GetAccount(accountId);
+            if (account.Balance + overdraftLimit < amount)
+            {
+                throw new OverDraftLimitExceededException($"Withdrawal of {amount} exceeds the overdraft limit of {overdraftLimit}.");
+            }
+
+            account.Balance -= amount;
+            accountRepository.UpdateAccount(account); // Save the updated balance
+        }
+
+        public void Deposit(long accountNumber, decimal amount)
+        {
+            // Retrieve the account
+            var account = accountRepository.GetAccountById(accountNumber);
+            if (account == null)
+            {
+                throw new InvalidAccountException($"Account with ID {accountNumber} does not exist.");
+            }
+
+            account.Balance += amount; // Increase balance
+            accountRepository.UpdateAccount(account); // Save the updated balance
+        }
+
+        public decimal GetBalance(long accountNumber)
+        {
+            var account = accountRepository.GetAccountById(accountNumber);
+            if (account == null)
+            {
+                throw new InvalidAccountException($"Account with ID {accountNumber} does not exist.");
+            }
+
+            return account.Balance; // Return the current balance
+        }
+
+        public void Transfer(long fromAccountNumber, long toAccountNumber, decimal amount)
+        {
+            var fromAccount = accountRepository.GetAccountById(fromAccountNumber);
+            var toAccount = accountRepository.GetAccountById(toAccountNumber);
+
+            if (fromAccount == null)
+            {
+                throw new InvalidAccountException($"Source account with ID {fromAccountNumber} does not exist.");
+            }
+
+            if (toAccount == null)
+            {
+                throw new InvalidAccountException($"Destination account with ID {toAccountNumber} does not exist.");
+            }
+
+            if (fromAccount.Balance < amount)
+            {
+                throw new InsufficientFundException($"Transfer amount {amount} exceeds the available balance of {fromAccount.Balance}.");
+            }
+
+            fromAccount.Balance -= amount;
+            toAccount.Balance += amount;
+
+            accountRepository.UpdateAccount(fromAccount);
+            accountRepository.UpdateAccount(toAccount);
+        }
+
+        public object GetAccountDetails(long accountNumber)
+        {
+            var account = accountRepository.GetAccountById(accountNumber);
+            if (account == null)
+            {
+                throw new InvalidAccountException($"Account with ID {accountNumber} does not exist.");
+            }
+
+            return account; // Return account details
+        }
+
+        public IEnumerable<object> ListAccounts()
+        {
+            return accountRepository.GetAllAccounts(); // Returns all accounts from repository
+        }
+
+        public void Withdraw(long accountNumber, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
